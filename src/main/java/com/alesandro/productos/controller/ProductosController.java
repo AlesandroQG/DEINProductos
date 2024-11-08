84fb577bd2a515a536983b9328565cb923165b96
Alesandro <116791603+AlesandroQuiros@users.noreply.github.com>
2024-11-08 09:46:14 +0100

    Creación fichero fxml

--- src/main/java/com/alesandro/productos/controller/ProductosController.java
+++ src/main/java/com/alesandro/productos/controller/ProductosController.java
@@ -1,8 +1,10 @@
 package com.alesandro.productos.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +13,42 @@ import java.util.ResourceBundle;
  * Clase que controla los eventos de la ventana principal
  */
 public class ProductosController implements Initializable {
+
+    @FXML // fx:id="btnActualizar"
+    private Button btnActualizar; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCrear"
+    private Button btnCrear; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cbDisponible"
+    private CheckBox cbDisponible; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCodigo"
+    private TableColumn<?, ?> colCodigo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colDisponible"
+    private TableColumn<?, ?> colDisponible; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colNombre"
+    private TableColumn<?, ?> colNombre; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colPrecio"
+    private TableColumn<?, ?> colPrecio; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imagen"
+    private ImageView imagen; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tabla"
+    private TableView<?> tabla; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtCodigo"
+    private TextField txtCodigo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtNombre"
+    private TextField txtNombre; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtPrecio"
+    private TextField txtPrecio; // Value injected by FXMLLoader
 
     /**
      * Función que se ejecuta cuando se inicia la ventana
@@ -21,6 +59,26 @@ public class ProductosController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //
+    }
+
+    @FXML
+    void actualizar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void limpiar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seleccionarImagen(ActionEvent event) {
+
     }
 
     /**
